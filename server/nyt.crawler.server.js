c76fab8a9fc612f21984c1e9500a114bf5ba2f25
Henryk Wollik <olafyoorsen@hotmail.com>
2012-10-26 18:55:58 +0200

    nodejs server works

--- server/nyt.crawler.server.js
+++ server/nyt.crawler.server.js
@@ -7,12 +7,81 @@
 //
 //
 
-var sys = require("sys");
-my_http = require("http");
-my_http.createServer(function(request,response){
-    sys.puts("I got kicked");
-    response.writeHeader(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8080);
-sys.puts("Server Running on 8080");
+var http     = require('http'),
+    https    = require('https');
+
+function NYTCrawler()
+{
+    this.__interval    = 2000;
+    this.__requestList = null;
+    this.__currentPass = -1;
+    this.__maxPass     = -1;
+    this.__timeout     = null;
+    this.__callback    = null;
+}
+
+NYTCrawler.prototype.reset = function()
+{
+    this.__interval    = 2000;
+    this.__requestList = null;
+    this.__currentPass = -1;
+    this.__maxPass     = -1;
+    this.__timeout     = null;
+    this.__callback    = null;
+};
+
+NYTCrawler.prototype.crawl = function()
+{
+
+};
+
+NYTCrawler.prototype.getJSON = function(callback)
+{
+    var options = {host:'api.nytimes.com',path:'/svc/search/v1/article?format=json&api-key=ac4190b51fdaa3113d81a263750fcf16:6:66636531'};
+    console.log(options.host+options.path);
+
+    http.get(options,function(res)
+    {
+        var json = "";
+
+        res.on('data',
+
+            function(chunk)
+            {
+                json += chunk;
+            }
+        );
+
+        res.on('end',
+
+            function()
+            {
+                if(res.statusCode == 200)
+                {
+                    console.log(json);
+                }
+            }
+        );
+    }).on('error',
+
+        function(e)
+        {
+            console.log('error: ' + e);
+        }
+
+    );
+};
+
+var crawler = new NYTCrawler();
+crawler.getJSON(null);
+
+
+
+
+
+
+
+
+
+
+
